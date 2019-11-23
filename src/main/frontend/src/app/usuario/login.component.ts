b069448912e21612151992f61fc02ad232a47c8b
ctejaxun <ctejaxun@infonet.com.gt>
2019-11-23 12:18:07 -0600

    agrega un plantilla def. para diseno login

--- src/main/frontend/src/app/usuario/login.component.ts
+++ src/main/frontend/src/app/usuario/login.component.ts
@@ -5,6 +5,7 @@ import { Usuario } from '../modelos';
 @Component({
     selector: "login"
     , templateUrl: "./login.component.html"
+    , styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
     ngOnInit(): void {
