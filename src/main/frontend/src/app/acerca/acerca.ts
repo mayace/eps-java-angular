1b24e15a4caeeb382d00b6a1c75262d385d82b3d
ctejaxun <ctejaxun@infonet.com.gt>
2019-11-25 00:09:34 -0600

    agrega componente acerca de

---
+++ src/main/frontend/src/app/acerca/acerca.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "acerca-de"
+    , templateUrl: "./acerca.html"
+})
+
+export class AcercaDeComponent implements OnInit {
+    ngOnInit(): void {
+    }
+
+
+}
